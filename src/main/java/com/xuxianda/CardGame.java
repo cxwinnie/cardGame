f1febdae54d32692f2555805d1f78b89fd9569e1
XuXianda <1114186632@qq.com>
2020-04-03 15:47:47 +0800

    Initial commit

---
+++ src/main/java/com/xuxianda/CardGame.java
@@ -0,0 +1,15 @@
+package com.xuxianda;
+
+/**
+ * Created by XiandaXu on 2020/4/3.
+ */
+public class CardGame {
+    
+    /**
+     * 发牌功能
+     */
+    public static void deliverCard(){
+
+    }
+
+}
---
+++ src/main/java/com/xuxianda/Test.java
@@ -0,0 +1,12 @@
+package com.xuxianda;
+
+/**
+ * Created by XiandaXu on 2020/4/3.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+
+}
